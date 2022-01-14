567450035a114bc3aeb97b06a96a932e5a68dcd6
William Saar <williamsaar@outlook.com>
2022-01-14 13:18:24 +0100

    Removed unnecessary files

--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,19 @@
 class Game {
 
-    private grid: Grid
+    
 
     constructor(){
-        this.grid = new Grid(createVector(10, 10))
+       
     }
 
     public update() {
-    this.grid.update()
+    
 
     }
 
     public draw() {
         background('white');
 
-        this.grid.draw()
+       
     }
 }
