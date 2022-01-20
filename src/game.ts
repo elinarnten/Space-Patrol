b83596f19e347bc76061b6b2e308b31a4ba4f44a
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-01-20 13:36:17 +0100

    Fix the countdown in topmenu Fixes #41

--- src/game.ts
+++ src/game.ts
@@ -17,7 +17,7 @@ class Game {
     
 
     constructor(level: Level, score: number){
-        this.level = new Level(1200, 1, 1, 1, 20, 3);
+        this.level = new Level(120, 1, 1, 1, 20, 3);
         this.pointSystem = new PointSystem(3)
         this.topMenu = new TopMenu(this.level, this.pointSystem);
         this.gameState = {gameState: "start"};
@@ -34,11 +34,14 @@ class Game {
     
     public update() {
         this.level.update();
+        this.topMenu.update();
+    
     }
 
 
     public draw() {
         this.level.draw();
+        this.level.LevelCountDownTimer();
     }
 
 }
