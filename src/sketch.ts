9e9cc915888887e0dd4145a6c63eb1f9c0bd192d
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-01-24 09:22:11 +0100

    Testing some collision + add score

--- src/sketch.ts
+++ src/sketch.ts
@@ -67,12 +67,7 @@ function keyTyped() {
      if (keyCode === 32 && spaceBar === false) {
          timerValue = 5
         stroke(random(255), random(100, 200), random(100), random(200, 255));
-         spaceBar = true;
-        
-         console.log(timerValue)
-         
-        
-         //kallar på isActivated funktionen    
+         spaceBar = true;   
      }
 
     
