15d3887c1323f902da1c6646e6016e16001ff4fc
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-01-24 15:16:43 +0100

    almost done with collision, beam fixed

--- src/LaserBeam.ts
+++ src/LaserBeam.ts
@@ -1,57 +1,65 @@
 
 
 class LaserBeam { //change time name
-    private time: number = 5000;
+    private time: number;
     private isActivated: boolean;
-    private hitsAsteroid: boolean; 
+    private hitsAsteroid: boolean;
     private angle: number;
     private position: p5.Vector;
     private angleChangeDirection: boolean;
-    // private endPos: {x:number, y:number} = {x:0,y:0};
+    private gameEngine: GameEngine;
+    private shootingBeam: IsActivated | null;
 
-    constructor() {
+    constructor(gameEngine: GameEngine) {
         this.isActivated = false;
         this.hitsAsteroid = false;
-        this.angle = 90;
-        this.position = createVector(width/2, height - 40);
+        this.angle = 0;
+        this.position = createVector(width / 2, height - 40);
         this.angleChangeDirection = true;
         this.time = 5000;
-        // isActivated = new IsActivated(false, angleBeam, createVector(width/2, height - 40));
+        this.gameEngine = gameEngine;
+        this.shootingBeam = null;
     }
 
     public update() {
         this.updateBeamAngle();
-        
+
         if (keyIsDown(32) && !this.isActivated) {
-            
             this.isActivated = true;
+            this.shootingBeam = new IsActivated(false, this.angle, createVector(width / 2, height - 40), this.gameEngine);
+            console.log(this.angle)
+            console.log('isac')
         }
-        
+
         if (this.isActivated) {
             this.time -= deltaTime;
             if (this.time < 0) {
                 this.time = 5000;
                 this.isActivated = false;
+                console.log('isnot')
+                this.shootingBeam = null;
             }
         }
     }
 
     private updateBeamAngle() {
-        if(this.angle < 90 && this.angleChangeDirection == true) {
-            this.angleChangeDirection = true;
-            
-        } else if (this.angle <= 90) {
-            this.angleChangeDirection = false
-        }
-
-        if(this.angle < 90 && this.angleChangeDirection == true && spaceBar == false) {
-            this.angle = this.angle + 1;
-        }   else if(this.angleChangeDirection == false && spaceBar == false) {
-            this.angle = this.angle - 1;
-            if (this.angle <= -90) {
+        if (!this.isActivated) {
+            if (this.angle < 0 && this.angleChangeDirection == true) {
                 this.angleChangeDirection = true;
+
+            } else if (this.angle <= 180) {
+                this.angleChangeDirection = false
             }
 
+            if (this.angle < 0 && this.angleChangeDirection == true && spaceBar == false) {
+                this.angle = this.angle + 1;
+            } else if (this.angleChangeDirection == false && spaceBar == false) {
+                this.angle = this.angle - 1;
+                if (this.angle <= -180) {
+                    this.angleChangeDirection = true;
+                }
+
+            }
         }
 
         // if (this.isActivated) {
@@ -62,19 +70,21 @@ class LaserBeam { //change time name
         //     }
         // }
     }
-    
+
 
     public draw() {
         push();
         strokeWeight(10);
-        translate(this.position.x, this.position.y);
-        rotate(this.angle)
-        line(0, 0, 0, -80)
+        
+        const endX = this.position.x+80* cos(this.angle);
+        const endY = this.position.y+80 * sin(this.angle);
+
+        line(width/2, height-40, endX, endY)
         pop()
 
-        // if(this.isActivated) {
-        //     isActivated.draw();
-        // }
+        if (this.isActivated && this.shootingBeam) {
+            this.shootingBeam.draw();
+        }
     }
 
 }
