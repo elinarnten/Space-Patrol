9f925e1ab7cce6708bc4020600354ced81f519fe
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-02-02 13:25:36 +0100

    fixed about text

--- src/StartMenu.ts
+++ src/StartMenu.ts
@@ -67,7 +67,7 @@ class StartMenu {
 
         const aboutContentDiv = document.createElement('div');
         aboutContentDiv.id = 'aboutContentDiv';
-        aboutContentDiv.innerHTML = 'This game takes place in space where dangerous asteroids are threatening our planet. You will be positioned in a spacecraft. Your weapon is a laserbeam. Save our planet by shooting down asteroids with the laser. Bigger asteroids needs more shots to die. Beware of the bombs: once you shoot on them, you will lose points. Aim carefully. Each level goes on time. You must meet the current number of meteorites to survive and to reach the next level. <br><br> Use the following keys: <br> ESC - Menu <br> P - Pause <br> ENTER - Start game <br> SPACEBAR - Shoot <br><br> Yours truly, <br><br> The Mother Earth Guardians: <br> Sabina Andersson, Elin Arntén, Sara Lindqvist, <br>Rosanna Pistone and William Saar';
+        aboutContentDiv.innerHTML = 'This game takes place in space where dangerous asteroids are threatening our planet. You will be positioned in a spacecraft. Your weapon is a laserbeam. Save our planet by shooting down asteroids with the laser. Bigger asteroids needs more shots to die. Beware of the bombs: if you shoot on them, you will lose life. Aim carefully. Each level goes on time. You must meet the needed score by shooting down asteroids to survive and to reach the next level. <br><br> Use the following keys: <br> ESC - Menu <br> P - Pause <br> ENTER - Start game <br> SPACEBAR - Shoot <br><br> Yours truly, <br><br> The Mother Earth Guardians: <br> Sabina Andersson, Elin Arntén, Sara Lindqvist, <br>Rosanna Pistone and William Saar';
         aboutGameInnerContainer.appendChild(aboutContentDiv); 
 
         const buttonDiv = document.createElement('div');
